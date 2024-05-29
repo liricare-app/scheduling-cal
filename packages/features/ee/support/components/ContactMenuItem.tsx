07f92930fe5c0b5cd1a10818410db87ee5aed3f7
safwanghamrawi <safwan.ghamrawi@gmail.com>
2024-05-29 23:09:38 +0200

    rm JOIN_DISCORD

--- packages/features/ee/support/components/ContactMenuItem.tsx
+++ packages/features/ee/support/components/ContactMenuItem.tsx
@@ -1,7 +1,6 @@
-import { JOIN_COMMUNITY } from "@calcom/lib/constants";
 import { useHasPaidPlan } from "@calcom/lib/hooks/useHasPaidPlan";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Icon, UpgradeTeamsBadge } from "@calcom/ui";
+import { UpgradeTeamsBadge } from "@calcom/ui";
 
 import FreshChatMenuItem from "../lib/freshchat/FreshChatMenuItem";
 import HelpscoutMenuItem from "../lib/helpscout/HelpscoutMenuItem";
@@ -29,16 +28,6 @@ export default function ContactMenuItem(props: ContactMenuItem) {
           <UpgradeTeamsBadge />
         </div>
       )}
-      <a
-        href={JOIN_COMMUNITY}
-        target="_blank"
-        className="hover:bg-subtle hover:text-emphasis text-default flex w-full px-5 py-2 pr-4 text-sm font-medium transition">
-        {t("community_support")}{" "}
-        <Icon
-          name="external-link"
-          className="group-hover:text-subtle text-muted ml-1 mt-px h-4 w-4 flex-shrink-0 ltr:mr-3"
-        />
-      </a>
     </>
   );
 }
