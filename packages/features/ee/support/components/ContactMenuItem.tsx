c79674633c16ff8d8bd62da74596d01535fd249a
safwanghamrawi <safwan.ghamrawi@gmail.com>
2024-05-29 23:09:38 +0200

    rm JOIN_DISCORD

--- packages/features/ee/support/components/ContactMenuItem.tsx
+++ packages/features/ee/support/components/ContactMenuItem.tsx
@@ -1,7 +1,7 @@
 import { JOIN_COMMUNITY } from "@calcom/lib/constants";
 import { useHasPaidPlan } from "@calcom/lib/hooks/useHasPaidPlan";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Icon, UpgradeTeamsBadge } from "@calcom/ui";
+import { UpgradeTeamsBadge } from "@calcom/ui";
 
 import FreshChatMenuItem from "../lib/freshchat/FreshChatMenuItem";
 import HelpscoutMenuItem from "../lib/helpscout/HelpscoutMenuItem";
